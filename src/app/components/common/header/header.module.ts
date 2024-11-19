02b1c53fa89a1f72ae93d9f8995dd34c60f795d3
Amalykyarima <91787678+Amalykyarima@users.noreply.github.com>
2024-11-19 10:18:09 +0100

    fix

--- src/app/components/common/header/header.module.ts
+++ src/app/components/common/header/header.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
 import { RouterModule } from '@angular/router';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+
 
 
 
 @NgModule({
-  declarations: [HeaderComponent],
+  declarations: [],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    NzDropDownModule
   ],
-  exports: [HeaderComponent]
+  exports: []
 })
 export class HeaderModule { }
